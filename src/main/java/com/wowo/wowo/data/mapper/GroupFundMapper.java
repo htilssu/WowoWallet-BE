2bac40f67efede578acc50a586daea2438d55185
htilssu <tolashuu@gmail.com>
2024-10-11 10:48:36 +0700

    add username and email for user

---
+++ src/main/java/com/wowo/wowo/data/mapper/GroupFundMapper.java
@@ -0,0 +1,17 @@
+package com.wowo.wowo.data.mapper;
+
+import com.wowo.wowo.data.dto.response.GroupFundDto;
+import com.wowo.wowo.models.GroupFund;
+import org.mapstruct.*;
+
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        componentModel = MappingConstants.ComponentModel.SPRING)
+public interface GroupFundMapper {
+
+    GroupFund toEntity(GroupFundDto groupFundDto);
+    GroupFundDto toDto(GroupFund groupFund);
+    @BeanMapping(
+            nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)GroupFund partialUpdate(
+            GroupFundDto groupFundDto,
+            @MappingTarget GroupFund groupFund);
+}
