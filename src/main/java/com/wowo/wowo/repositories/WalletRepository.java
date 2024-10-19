ee7226442ed3928ed0519942c548acb1dbedcb67
htilssu <tolashuu@gmail.com>
2024-10-19 17:34:44 +0700

    fix

--- src/main/java/com/wowo/wowo/repositories/WalletRepository.java
+++ src/main/java/com/wowo/wowo/repositories/WalletRepository.java
@@ -1,16 +1,17 @@
 package com.wowo.wowo.repositories;
 
 import com.wowo.wowo.models.Wallet;
+import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
-public interface WalletRepository extends CrudRepository<Wallet, String> {
+public interface WalletRepository extends CrudRepository<Wallet, Long> {
 
     Optional<Wallet> findByOwnerIdAndOwnerType(String ownerId, String ownerType);
     Optional<Wallet> findByOwnerId(String senderId);
     boolean existsByOwnerId(String ownerId);
-    boolean existsByOwnerIdAndId(String ownerId, Integer id);
+    boolean existsByOwnerIdAndId(@Size(max = 32) String ownerId, Long id);
 }
