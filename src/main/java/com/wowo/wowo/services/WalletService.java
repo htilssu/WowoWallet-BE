93dcea35f5636af7ecfd09f968852fd99bf3e47f
htilssu <tolashuu@gmail.com>
2024-10-19 17:39:42 +0700

    aa

--- src/main/java/com/wowo/wowo/services/WalletService.java
+++ src/main/java/com/wowo/wowo/services/WalletService.java
@@ -16,7 +16,7 @@ public class WalletService {
     private final WalletRepository walletRepository;
 
     public WalletResponse getWallet(int id) {
-        var wallet = walletRepository.findById(String.valueOf(id))
+        var wallet = walletRepository.findById((long) id)
                 .orElseThrow(() -> new RuntimeException("Không tìm thấy ví"));
 
 
@@ -37,11 +37,11 @@ public class WalletService {
     }
 
     public boolean isWalletOwner(String userId, int walletId) {
-        return walletRepository.existsByOwnerIdAndId(userId, walletId);
+        return walletRepository.existsByOwnerIdAndId(userId, (long) walletId);
     }
 
     public boolean isWalletOwner(String userId, String walletId) {
         final int walletIdInt = Integer.parseInt(walletId);
-        return walletRepository.existsByOwnerIdAndId(userId, walletIdInt);
+        return walletRepository.existsByOwnerIdAndId(userId, (long) walletIdInt);
     }
 }
