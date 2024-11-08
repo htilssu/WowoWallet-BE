0c5b8a41910abc1bae9eb9c19b0095d2c603d187
htilssu <tolashuu@gmail.com>
2024-11-08 08:05:00 +0700

    simplify code

--- src/main/java/com/wowo/wowo/controllers/TransactionController.java
+++ src/main/java/com/wowo/wowo/controllers/TransactionController.java
@@ -1,11 +1,11 @@
 package com.wowo.wowo.controllers;
 
+import com.wowo.wowo.annotations.authorized.IsAdmin;
 import com.wowo.wowo.annotations.authorized.IsUser;
-import com.wowo.wowo.data.dto.TransactionDto;
 import com.wowo.wowo.data.dto.PagingDto;
+import com.wowo.wowo.data.dto.TransactionDto;
 import com.wowo.wowo.data.dto.TransactionHistoryResponseDto;
 import com.wowo.wowo.data.mapper.TransactionMapper;
-import com.wowo.wowo.models.Transaction;
 import com.wowo.wowo.repositories.TransactionRepository;
 import com.wowo.wowo.services.TransactionService;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -37,44 +37,33 @@ public class TransactionController {
     @GetMapping("/history")
     public TransactionHistoryResponseDto getAllTransaction(@ModelAttribute @Validated PagingDto allParams,
             Authentication authentication) {
+        return getTransactionHistory(allParams, authentication.getPrincipal().toString());
+    }
+
+    private TransactionHistoryResponseDto getTransactionHistory(PagingDto allParams,
+            String userId) {
         int offset = allParams.getOffset();
         int page = allParams.getPage();
         offset = Math.min(30, Math.max(offset, 0));
 
         final List<TransactionDto> recentTransactions = transactionService.getRecentTransactions(
-                ((String) authentication.getPrincipal()),
+                (userId),
                 offset,
                 page);
 
         long total = transactionService.getTotalTransactions(
-                ((String) authentication.getPrincipal()));
+                (userId));
         return new TransactionHistoryResponseDto(recentTransactions, total);
     }
-    //lấy lịch sử giao dịch của 1 user nào đó trong hệ thống
+
+//    @IsAdmin
     @GetMapping("/{userId}/history")
-    public ResponseEntity<TransactionHistoryResponseDto> getAllTransactionsByUserId(
+    public TransactionHistoryResponseDto getAllTransactionsByUserId(
             @ModelAttribute @Validated PagingDto allParams,
             @PathVariable String userId) {
 
-        // Lấy các tham số phân trang từ PagingDto
-        int offset = allParams.getOffset();
-        int page = allParams.getPage();
-
-        // Đảm bảo offset không vượt quá 30 và không nhỏ hơn 0
-        offset = Math.min(30, Math.max(offset, 0));
-
-        // Lấy danh sách giao dịch gần đây của người dùng
-        final List<TransactionDto> recentTransactions = transactionService.getRecentTransactions(
-                userId, offset, page);
-
-        // Lấy tổng số giao dịch của người dùng
-        long total = transactionService.getTotalTransactions(userId);
-
-        TransactionHistoryResponseDto response = new TransactionHistoryResponseDto(recentTransactions, total);
-
-        return ResponseEntity.ok(response);
+        return getTransactionHistory(allParams, userId);
     }
-
 
     @PostMapping("{id}/refund")
     public ResponseEntity<?> refundTransaction(@PathVariable String id) {
