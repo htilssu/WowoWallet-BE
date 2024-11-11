f925003c64779bb972504bd3889f19a3698ff2da
RainPauL <142281503+catchmeifyoucan99@users.noreply.github.com>
2024-11-11 10:21:52 +0700

    Feat/verify (#73)
    * fix code/done test
    
    * pull
    
    * done
    
    * verify
    
    * verify version

---
+++ src/main/java/com/wowo/wowo/data/dto/VerifyDto.java
@@ -0,0 +1,33 @@
+package com.wowo.wowo.data.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor 
+public class VerifyDto {
+    private Long id;
+    private Customer customer; 
+    private String type;
+    private Long numberCard;
+    private LocalDateTime openDay;
+    private LocalDateTime closeDay;
+    private String fontImage;
+    private String behindImage;
+    private String userImage;
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor 
+    public static class Customer {
+        private String id;  
+    }
+}
