0c46281646b731813625a1a661f2758a405d2b14
htilssu <tolashuu@gmail.com>
2024-11-13 13:22:35 +0700

    update

--- src/main/java/com/wowo/wowo/kafka/messages/VoucherMessage.java
+++ src/main/java/com/wowo/wowo/kafka/messages/VoucherMessage.java
@@ -18,33 +18,7 @@ import com.wowo.wowo.models.PaymentStatus;
 
 import java.util.Objects;
 
-public final class VoucherMessage {
-
-    private final String voucherId;
-    private final PaymentStatus status;
-
-    public VoucherMessage(String voucherId, PaymentStatus status) {
-        this.voucherId = voucherId;
-        this.status = status;
-    }
-
-    public String voucherId() {return voucherId;}
-
-    public PaymentStatus status() {return status;}
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this) return true;
-        if (obj == null || obj.getClass() != this.getClass()) return false;
-        var that = (VoucherMessage) obj;
-        return Objects.equals(this.voucherId, that.voucherId) &&
-                Objects.equals(this.status, that.status);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(voucherId, status);
-    }
+public record VoucherMessage(String voucherId, PaymentStatus status) {
 
     @Override
     public String toString() {
