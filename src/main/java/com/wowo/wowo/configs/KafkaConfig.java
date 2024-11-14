711c4527679280a534d56eb8e8703df09782f952
htilssu <tolashuu@gmail.com>
2024-11-14 22:52:43 +0700

    add kafka error handler

---
+++ src/main/java/com/wowo/wowo/configs/KafkaConfig.java
@@ -0,0 +1,39 @@
+/*
+ * ******************************************************
+ *  * Copyright (c) 2024 htilssu
+ *  *
+ *  * This code is the property of htilssu. All rights reserved.
+ *  * Redistribution or reproduction of any part of this code
+ *  * in any form, with or without modification, is strictly
+ *  * prohibited without prior written permission from the author.
+ *  *
+ *  * Author: htilssu
+ *  * Created: 14-11-2024
+ *  ******************************************************
+ */
+
+package com.wowo.wowo.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.util.backoff.FixedBackOff;
+
+@Configuration
+public class KafkaConfig {
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory(
+            ConsumerFactory consumerFactory) {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory);
+        factory.getContainerProperties()
+                .setAckMode(ContainerProperties.AckMode.RECORD);
+        factory.setCommonErrorHandler(new DefaultErrorHandler(new FixedBackOff(1000L, 2L)));
+        return factory;
+    }
+}
