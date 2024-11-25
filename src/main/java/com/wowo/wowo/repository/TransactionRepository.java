8ed18caa25d6da2f208fc1d5ff8c2aa4601d9215
htilssu <tolashuu@gmail.com>
2024-11-25 22:34:17 +0700

    fix query with same receive wallet

--- src/main/java/com/wowo/wowo/repository/TransactionRepository.java
+++ src/main/java/com/wowo/wowo/repository/TransactionRepository.java
@@ -11,8 +11,8 @@ import java.util.List;
 
 public interface TransactionRepository extends JpaRepository<Transaction, String> {
 
-    @Query("SELECT t FROM Transaction t, GroupFund g WHERE t.receiveWallet.id = g.wallet.id OR t" +
-            ".receiveWallet.id = g.wallet.id AND g.id = :groupId ORDER BY t.updated DESC")
+    @Query("SELECT t FROM Transaction t, GroupFund g WHERE (t.senderWallet.id = g.wallet.id OR t" +
+            ".receiveWallet.id = g.wallet.id) AND g.id = :groupId ORDER BY t.updated DESC")
     List<Transaction> getGroupFundTransaction(Long groupId, Pageable pageable);
     List<Transaction> findTransactionsByReceiveWalletOrSenderWallet(@NotNull Wallet receiveWallet, @NotNull Wallet senderWallet, Pageable pageable);
     long countTransactionBySenderWalletOrReceiveWallet(@NotNull Wallet senderWallet, @NotNull Wallet receiveWallet);
