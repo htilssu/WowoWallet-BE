d57e18776659970c9b3a4a7b149f406c3e3fef24
htilssu <tolashuu@gmail.com>
2024-11-26 21:16:53 +0700

    add top up limit

--- src/main/java/com/wowo/wowo/service/TopUpService.java
+++ src/main/java/com/wowo/wowo/service/TopUpService.java
@@ -14,12 +14,14 @@
 
 package com.wowo.wowo.service;
 
-import com.paypal.sdk.models.Order;
+import com.wowo.wowo.constant.Constant;
 import com.wowo.wowo.exception.BadRequest;
+import com.wowo.wowo.exception.NotFoundException;
 import com.wowo.wowo.model.FlowType;
 import com.wowo.wowo.model.TopUpRequest;
 import com.wowo.wowo.model.Transaction;
 import com.wowo.wowo.model.Wallet;
+import com.wowo.wowo.repository.ConstantRepository;
 import com.wowo.wowo.repository.TopUpRequestRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -32,11 +34,15 @@ public class TopUpService {
     private final WalletService walletService;
     private final TransferService transferService;
     private final TransactionService transactionService;
+    private final ConstantRepository constantRepository;
 
     public Wallet topUpWithLimit(String orderId) {
         final TopUpRequest order = topUpRequestRepository.findByOrderId(
                         orderId)
                 .orElseThrow(() -> new BadRequest("Order not found"));
+        if (!checkLimit(order.getAmount())) {
+            throw new BadRequest("Số tiền nạp vượt quá giới hạn");
+        }
         final Wallet wallet = walletService.getWallet(Long.valueOf(order.getWalletId()));
         transferService.transferWithNoFee(walletService.getRootWallet(), wallet, order.getAmount());
         Transaction transaction = new Transaction();
@@ -50,7 +56,19 @@ public class TopUpService {
         return wallet;
     }
 
+    public boolean checkLimit(long amount) {
+        final com.wowo.wowo.model.Constant constant = constantRepository.findById(
+                        Constant.MAXIMUM_TOP_UP_AMOUNT)
+                .orElseThrow(() -> new NotFoundException("Constant not found"));
+
+        return amount <= constant.getValue();
+    }
+
     public Wallet topUpWithLimit(String walletId, long amount) {
+        if (!checkLimit(amount)) {
+            throw new BadRequest("Số tiền nạp vượt quá giới hạn");
+        }
+
         final Wallet wallet = walletService.getWallet(Long.valueOf(walletId));
         final Wallet rootWallet = walletService.getRootWallet();
         transferService.transferWithNoFee(rootWallet, wallet, amount);
@@ -64,5 +82,4 @@ public class TopUpService {
         transactionService.save(transaction);
         return wallet;
     }
-
 }
