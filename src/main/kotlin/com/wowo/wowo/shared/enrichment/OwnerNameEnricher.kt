f3b8bd1d61349049757640c4f6a37664d3d83dbe
Trung Hieu <trunghieu.dev88@gmail.com>
2026-03-24 23:14:25 +0700

    Enhance OwnerNameEnricher to support group fund owner name enrichment

--- src/main/kotlin/com/wowo/wowo/shared/enrichment/OwnerNameEnricher.kt
+++ src/main/kotlin/com/wowo/wowo/shared/enrichment/OwnerNameEnricher.kt
@@ -1,13 +1,17 @@
 package com.wowo.wowo.shared.enrichment
 
+import com.wowo.wowo.contexts.groupfund.domain.*
+import com.wowo.wowo.contexts.groupfund.infrastructure.persistence.*
 import com.wowo.wowo.contexts.user.domain.repository.*
-import com.wowo.wowo.contexts.user.domain.valueobject.UserId
+import com.wowo.wowo.contexts.user.domain.valueobject.*
 import com.wowo.wowo.shared.domain.*
 import org.springframework.stereotype.*
-import java.util.UUID
+import java.util.*
 
 @Component
-class OwnerNameEnricher(val userRepository: UserRepository) : IEnricher<IHasOwner<String>> {
+class OwnerNameEnricher(
+    val userRepository: UserRepository, private val groupFundRepositoryAdapter: GroupFundRepositoryAdapter
+) : IEnricher<IHasOwner<String>> {
 
 
     override fun enrich(data: IHasOwner<String>): IHasOwner<String> {
@@ -16,6 +20,13 @@ class OwnerNameEnricher(val userRepository: UserRepository) : IEnricher<IHasOwne
                 val user = userRepository.findById(UserId(UUID.fromString(data.ownerId)))
                 if (user != null) {
                     data.ownerName = user.username.value
+                }
+            }
+
+            OwnerType.FUND_GROUP -> {
+                val groupFund = groupFundRepositoryAdapter.findById(GroupId(UUID.fromString(data.ownerId)))
+                if (groupFund != null) {
+                    data.ownerName = groupFund.name
                 }
             }
 
